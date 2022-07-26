96a26a85860ab6647bbfac5bb4c7e02591a45da8
CHA TAE JUN <cha7713@gmail.com>
2022-07-26 11:26:59 +0900

    플랫폼 버전 지정

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SecureMarkIOS",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "SecureMarkIOS", targets: ["SecureMarkIOS"])
