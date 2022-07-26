d54d86e131abd5cf3fd3252b98ff5e3aa800423a
CHA TAE JUN <cha7713@gmail.com>
2022-07-26 17:46:41 +0900

    -

--- Sources/SecureMarkIOS/view/WaterMarkView.swift
+++ Sources/SecureMarkIOS/view/WaterMarkView.swift
@@ -42,7 +42,7 @@ struct WaterMarkView: View {
                             }
                         }
                     }
-                }.edgesIgnoringSafeArea(.all)
+                }.ignoresSafeArea()
             }
         }
     }
