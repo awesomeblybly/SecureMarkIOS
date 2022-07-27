e40198ce868c6dc8b7b312d771d54949ea4d2ae1
CHA TAE JUN <cha7713@gmail.com>
2022-07-27 14:46:37 +0900

    -

--- Sources/SecureMarkIOS/view/WaterMarkView.swift
+++ Sources/SecureMarkIOS/view/WaterMarkView.swift
@@ -91,7 +91,7 @@ extension String{
     func multiply(_ time: Int) -> String {
         var temp = ""
         for _ in 0...time {
-            temp += "\t\(self)\t"
+            temp += "  \(self)  "
         }
         return temp
     }
